3897839253f5139ea99c289408c753f5f687e921
minjae0402 <kangjack0402@gmail.com>
2024-11-08 18:27:00 +0900

    Study : foreach

--- Program.cs
+++ Program.cs
@@ -3,6 +3,44 @@
 namespace HelloWorld {
   class Program {
     static void Main(string[] args) {
+      int[,] arrs = new int[2, 3] {
+        {1, 2, 3},
+        {4, 5, 6}
+      };
+
+      foreach(var arrsin in arrs) {
+        Console.WriteLine("arrs : " + arrsin);
+      }
+
+
+      int[] arr = new int[5];
+      List<int> list = new List<int>() {
+        1, 2, 3, 4
+      };
+
+      Dictionary<int, int> list2 = new Dictionary<int, int>() {
+        {1, 2},
+        {3, 4}
+      };
+      foreach(var lit2 in list2) {
+        Console.WriteLine("List2 : " + lit2);
+      }
+
+      for (int i = 0; i < 5; i++) {
+        arr[i] = i+1;
+      }
+      for (int i = 0; i < 5; i++) {
+        Console.WriteLine(arr[i]);
+      }
+      
+      foreach(var num in arr) {
+        Console.WriteLine(num);
+      }
+
+      foreach(var lit in list) {
+        Console.WriteLine("list : " + lit);
+      }
+
       string HelloWorld = "HelloWorld";
       Console.WriteLine(HelloWorld);
     }
--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95db5a517af9742edf5c75a79844c5dbb0fc05e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
