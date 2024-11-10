fcf5286f43266e87e0a02d49d24c281395250086
minjae0402 <kangjack0402@gmail.com>
2024-11-10 21:52:32 +0900

    none

--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-
-class Person {
-  public string Name;
-  public void Eat() {
-    Console.WriteLine(Name + "(이)기 식사를 합니다.");
-  }
-}
+using System.Globalization;
 
 class Program {
   static void Main(string[] args) {
-      Person p1 = new Person();
-      p1.Name = "민재";
-      p1.Eat();
-    }
+    
+  }
 }
--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3897839253f5139ea99c289408c753f5f687e921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330a6d91c90684e90086e9a23cdd65505e73374a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
