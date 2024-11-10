ac3e5df2e1e7b60fda904d610f525944609d411f
minjae0402 <kangjack0402@gmail.com>
2024-11-10 23:51:37 +0900

    Collections

--- Program.cs
+++ Program.cs
@@ -1,20 +1,44 @@
 ﻿using System;
+using System.Collections;
 
 class Program {
   static void Main() {
-    int[] arr = new int[] {
-      1, 2, 3
-    };
-    for (int i = 0; i < arr.Length; i++) {
-      arr[i] = i+i;
+    ArrayList al = new ArrayList();
+    Queue qu = new Queue();
+    Stack st = new Stack();
+    Hashtable ht = new Hashtable();
+
+    al.Add(1);
+    al.Add("Hello");
+    al.Add(1.1);
+    al.Add(true);
+
+    qu.Enqueue(1);
+    qu.Enqueue(2);
+    qu.Enqueue(3);
+
+    st.Push(1);
+    st.Push(2);
+    st.Push(3);
+
+    ht["apple"] = "사과";
+    ht["banana"] = "바나나";
+    ht["orange"] = "오렌지";
+
+    foreach(var item in al) {
+      Console.WriteLine("ArrayList : " + item);
     }
 
-    
-    for (int i = 0; i < arr.Length; i++) {
-      Console.WriteLine(arr[i]);
+    while (qu.Count > 0) {
+      Console.WriteLine("Queue : " + qu.Dequeue());
     }
-    foreach (var i in arr) {
-      Console.WriteLine(i);
+
+    while (st.Count > 0) {
+      Console.WriteLine("Stack : " + st.Pop());
     }
+
+    Console.WriteLine(ht["apple"]);
+    Console.WriteLine(ht["banana"]);
+    Console.WriteLine(ht["orange"]);
   }
 }
